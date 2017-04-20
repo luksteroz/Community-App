0556299dea504ee55841cf069fc424eae6d208e9
luksteroz <lukasz.jeleniewicz@gmail.com>
2017-04-20 12:04:32 +0200

    first commit

---
+++ js/project.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Todo extends React.Component {
+  render() {
+    return
+  }
+}
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
