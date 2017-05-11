7298d2880036ee45db0c448074b6f62f3c9e41a3
Łukasz Jeleniewicz <lukasz.jeleniewicz@gmail.com>
2017-05-11 22:05:36 +0200

    Update Header.jsx

--- js/Header.jsx
+++ js/Header.jsx
@@ -5,9 +5,7 @@ import React from 'react';
 class Header extends React.Component {
     render(){
         return <header>
-            <span>COMMUNITY</span>
             <div id="logo"></div>
-            <span>WINDOW</span>
         </header>
     }
 }
